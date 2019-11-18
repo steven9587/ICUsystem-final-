2f59a7289d1c5832de48ec0aa9b354227df61899
steven9587 <steven9587@gmail.com>
2019-11-18 15:26:26 +0800

    first commit

---
+++ HISMvcProject1/HISMvcProject1/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HIS.Entity.Models;
+using HIS.Entity.ViewModels;
+using HIS.Mvc;
+using HIS.Mvc.Controller;
+using HIS.Systems;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+
+namespace HISMvcProject1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
